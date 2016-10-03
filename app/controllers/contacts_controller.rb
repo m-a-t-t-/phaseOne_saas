b49b3510e0adef94f19cc0efe292c814c924d67f
Matthew King <matthewlewisking@gmail.com>
2016-10-03 03:06:05 +0000

    Created contact form view, controller, added custom styles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController #function that adds what we want same as in the model file
     def new
+        @contact = Contact.new #this creates a object in the servers memory (blanks comntact object) we use this in the view file, @contact must be declared here
     end
     
     def create
