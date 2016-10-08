d48308d3af492a91ec4acbe2c889a0a143103b43
Matthew King <matthewlewisking@gmail.com>
2016-10-08 00:44:31 +0000

    Added association for plans and users

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ActiveRecord::Base
-    
+    has_many :users #symbol syntax - associates plan with user
 end
 #application is now aware that it has a plan object
 #an object is a list of attributes that are related to a certain type of real-world 'thing'
