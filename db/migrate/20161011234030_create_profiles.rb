b7954083d67659831d4b1dfd66eb0161c292e2eb
Matthew King <matthewlewisking@gmail.com>
2016-10-23 21:09:41 +0000

    changed comment location

--- db/migrate/20161011234030_create_profiles.rb
+++ db/migrate/20161011234030_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
-    create_table :profiles do |t| #inter column, this will tie the databases together
-      t.integer :user_id
+    create_table :profiles do |t| 
+      t.integer :user_id #inter column, this will tie the databases together
       t.string :first_name
       t.string :last_name
       t.string :job_title
