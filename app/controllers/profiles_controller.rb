1de3e97699a93a171c2b0a069ec9ed0a2e4f88cb
Matthew King <matthewlewisking@gmail.com>
2016-10-23 22:12:55 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
-    
+    def new
+       # form for user can create unique profile
+       @user = User.find( params[:user_id] )
+       # established nested action, this allows us to use the build_profile
+       @profile = @user.build_profile
+       
+    end
 end
