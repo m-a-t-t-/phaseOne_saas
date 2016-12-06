a196040c4cc82e03981413db0c49eb8af12ed2f0
Matthew King <matthewlewisking@gmail.com>
2016-12-06 22:51:50 +0000

    minor tweaks to notes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
     def new
        # form for user can create unique profile
+       # params that accesses specific id is how we only show the user their own profile
        @user = User.find( params[:user_id] )
        # established nested action, this allows us to use the build_profile
        @profile = @user.build_profile
