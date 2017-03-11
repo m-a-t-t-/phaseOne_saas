b24263f457a3ecae5f35682915483e7b868efbaa
Matthew King <matthewlewisking@gmail.com>
2017-03-11 23:10:53 +0000

    added first round of styles to user show page and added user helper page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,10 +14,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
         end
     end
     private
-        def select_plan
-            unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
-              flash[:notice] = "Please select a membership plan to sign up."
-              redirect_to root_url
-            end
+    def select_plan
+        unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+          flash[:notice] = "Please select a membership plan to sign up."
+          redirect_to root_url
         end
+    end
 end
