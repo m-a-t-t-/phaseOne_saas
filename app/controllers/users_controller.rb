cb473a915543067c201bc7df558d0c6b33d83378
Matthew King <matthewlewisking@gmail.com>
2017-04-12 23:01:44 +0000

    added notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
    before_action :authenticate_user!
    
+   def index #list all resources in the UserController
+   end
+   
    def show
        @user = User.find( params[:id] )
    end
