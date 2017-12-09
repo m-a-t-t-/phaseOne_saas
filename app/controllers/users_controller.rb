a925b73a52f8cea85e5812c5370cc1c167f09f63
Matthew King <matthewlewisking@gmail.com>
2017-12-09 02:18:27 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
    before_action :authenticate_user!
    
    def index #list all resources in the UserController
+      @users = User.all #array object storing all users
    end
    
    def show
